28d3fb62a12af3aa5673272e8958c023e4866640
Nico Kratky <nico@nicokratky.me>
2018-01-19 10:24:15 +0100

    added link info map

--- include/router/router.h
+++ include/router/router.h
@@ -9,12 +9,16 @@
  * class: 5cHIF
  */
 
+#ifndef DIVE_ROUTER_ROUTER_H
+#define DIVE_ROUTER_ROUTER_H
+
 /*
  * C++ system files
  */
 #include <iostream>
 #include <map>
 #include <string>
+#include <memory>
 
 /*
  * Vendor header files
@@ -24,16 +28,45 @@
 #include "json.hpp"
 
 /*!
- * \class Router
+ * \brief Stores link info
+ */
+struct Link {
+    std::string ip_address;
+    int port;
+
+    /*!
+     * \brief The router to which a packet has to be sent to reach the final
+     *        destination.
+     */
+    std::string next_hop;
+
+    /*!
+     * \brief Default constructor
+     */
+    Link() {}
+
+    /*!
+     * \brief Copy constructor
+     */
+    Link(const Link& l) :
+        ip_address{l.ip_address},
+        port{l.port},
+        next_hop{l.next_hop} {}
+
+    Link(std::string i, int p) :
+        ip_address{i}, port{p}, next_hop{} {
+    }
+};
+
+/*!
  * \brief Class to simulate a router
  */
 class Router {
-
   public:
-
-    Router(std::string& router_id, std::string& ip_address, int port);
-    Router(std::string& router_id, std::string& ip_address, int port,
-           std::shared_ptr<spdlog::logger> logger);
+    Router(const std::string& router_id, const std::string& ip_address,
+           int port);
+    Router(const std::string& router_id, const std::string& ip_address,
+           int port, std::shared_ptr<spdlog::logger> logger);
 
     /*!
     * \brief Set the cost that it takes to reach a router
@@ -43,7 +76,7 @@ class Router {
     *
     * \return void
     */
-    void set_cost(std::string& router_id, int cost);
+    void set_cost(const std::string& router_id, int cost);
 
     /*!
     * \brief Initialize the distance vector from a json file
@@ -71,5 +104,7 @@ class Router {
     int port_;
 
     std::map<std::string, int> distance_vector_;
-
+    std::map<std::string, Link> links_;
 };
+
+#endif  // DIVE_ROUTER_ROUTER_H
--- src/router/router.cpp
+++ src/router/router.cpp
@@ -16,6 +16,7 @@
  */
 #include <iostream>
 #include <string>
+#include <memory>
 
 /*
  * Vendor header files
@@ -47,21 +48,39 @@ void Router::set_cost(std::string& router_id, int cost) {
 
 
 void Router::initialize_from_json(json nodes, json links) {
+    // Loop through all nodes and store there connection informations
     for (json::iterator it = nodes.begin(); it != nodes.end(); ++it) {
         if (it.key() == router_id_) {
             distance_vector_[router_id_] = 0;
         } else {
             distance_vector_[it.key()] = -1;
+
+            std::string ip_address{
+                nodes[it.key()]["ip_address"].get<std::string>()};
+            int port{nodes[it.key()]["port"].get<int>()};
+
+            Link l{ip_address, port};
+            links_[it.key()] = l;
         }
     }
 
+    // Loop through all links and store neighbours
     for (const auto& link : links) {
+        std::string target;
         if (link["source"] == router_id_) {
             // link is a neighbour
-            distance_vector_[link["target"].get<std::string>()] = 1;
+            target = link["target"].get<std::string>();
+
+            distance_vector_[target] = 1;
+
+            links_[target].next_hop = router_id_;
         } else if (link["target"] == router_id_) {
             // link is a neighbour
-            distance_vector_[link["source"].get<std::string>()] = 1;
+            target = link["source"].get<std::string>();
+
+            distance_vector_[target] = 1;
+
+            links_[target].next_hop = router_id_;
         }
     }
 }
@@ -81,7 +100,12 @@ std::ostream& operator<<(std::ostream& os, const Router& r) {
     for (const auto& node : r.distance_vector_) {
         os << fmt::format("{:<5}", node.second);
     }
-    os << std::endl;
+    os << std::endl << std::endl;
+
+    for (const auto& link : r.links_) {
+        os << fmt::format("{:5}: {}", link.first,
+                                      link.second.next_hop) << std::endl;
+    }
 
     return os;
 }
